d9555b5c2c32f762428348c400d008130c15d58e
keren_g <keren.gruteke@wiliot.com>
2023-06-24 11:38:08 +0300

    format file

--- learn_decoder.py
+++ learn_decoder.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """ learn_decoder """
-import sys
 
 import numpy as np
 import sklearn.linear_model
+
 
 def read_matrix(filename, sep=","):
     lines = []
@@ -12,22 +12,36 @@ def read_matrix(filename, sep=","):
             lines.append(list(map(float, line.strip().split(sep))))
     return np.array(lines)
 
+
 data = read_matrix("imaging_data.csv", sep=",")
 vectors = read_matrix("vectors_180concepts.GV42B300.txt", sep=" ")
 
+
 def learn_decoder(data, vectors):
-     """ Given data (a CxV matrix of V voxel activations per C concepts)
-     and vectors (a CxD matrix of D semantic dimensions per C concepts)
-     find a matrix M such that the dot product of M and a V-dimensional 
-     data vector gives a D-dimensional decoded semantic vector. 
+    """Given data (a CxV matrix of V voxel activations per C concepts)
+    and vectors (a CxD matrix of D semantic dimensions per C concepts)
+    find a matrix M such that the dot product of M and a V-dimensional
+    data vector gives a D-dimensional decoded semantic vector.
 
-     The matrix M is learned using ridge regression:
-     https://en.wikipedia.org/wiki/Tikhonov_regularization
-     """
-     ridge = sklearn.linear_model.RidgeCV(
-         alphas=[1, 10, .01, 100, .001, 1000, .0001, 10000, .00001, 100000, .000001, 1000000],
-         fit_intercept=False
-     )
-     ridge.fit(data, vectors)
-     return ridge.coef_.T
-
+    The matrix M is learned using ridge regression:
+    https://en.wikipedia.org/wiki/Tikhonov_regularization
+    """
+    ridge = sklearn.linear_model.RidgeCV(
+        alphas=[
+            1,
+            10,
+            0.01,
+            100,
+            0.001,
+            1000,
+            0.0001,
+            10000,
+            0.00001,
+            100000,
+            0.000001,
+            1000000,
+        ],
+        fit_intercept=False,
+    )
+    ridge.fit(data, vectors)
+    return ridge.coef_.T
